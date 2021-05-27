f0715c5a1221f53a141ff0758236505cbfcbe351
lukhen <lukasz.hen@gmail.com>
2021-05-27 22:49:32 +0200

    We can now parse arbitrary Comm1 command as long as it has all options.

--- src/index.test.ts
+++ src/index.test.ts
@@ -15,13 +15,21 @@ interface Command2 {
 
 
 
+function getO2(argv: string[]): string {
+    return argv[argv.findIndex(el => el == "--o2") + 1]
+}
+
+function getO1(argv: string[]): string {
+    return argv[argv.findIndex(el => el == "--o1") + 1]
+}
+
 function parseArgv(argv: Array<string>): Command1 {
     return {
 
         _tag: "comm1",
         arg: argv[1],
-        o1: argv[3],
-        o2: argv[5]
+        o1: getO1(argv),
+        o2: getO2(argv)
     }
 }
 
@@ -47,4 +55,16 @@ describe("", () => {
         expect(parseArgv(["comm1", "arg2", "--o1", "someoption11", "--o2", "someoption22"]))
             .toEqual(comm1)
     })
+
+    test("", () => {
+        const comm1: Command1 = {
+            _tag: "comm1",
+            arg: "arg2",
+            o1: "someoption11",
+            o2: "someoption22"
+        }
+        expect(parseArgv(["comm1", "arg2", "--o2", "someoption22", "--o1", "someoption11"]))
+            .toEqual(comm1)
+    })
+
 })
