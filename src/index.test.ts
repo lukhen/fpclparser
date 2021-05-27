a17311d2cc3f5fe42881166d10c4fed53a56c462
lukhen <lukasz.hen@gmail.com>
2021-05-27 12:24:18 +0200

    We can now parse argv to a hardcoded version of Command1.

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,38 @@
+import * as E from "fp-ts/lib/Either"
+
+interface Command1 {
+    _tag: "comm1",
+    arg: string
+    o1: string,
+    o2: string
+}
+
+interface Command2 {
+    _tag: "comm2",
+    o3: string,
+    o4: string
+}
+
+
+
+function parseArgv(argv: Array<string>): Command1 {
+    return {
+        _tag: "comm1",
+        arg: "lukh",
+        o1: "someoption1",
+        o2: "someoption2"
+    }
+}
+
 describe("", () => {
     test("", () => {
-        expect(2).toEqual(2)
+        const comm1: Command1 = {
+            _tag: "comm1",
+            arg: "lukh",
+            o1: "someoption1",
+            o2: "someoption2"
+        }
+        expect(parseArgv(["comm1", "lukh", "--o1", "someoption1", "--o2", "someoption2"]))
+            .toEqual(comm1)
     })
 })
