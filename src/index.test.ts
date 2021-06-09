eb3fb9eda623c64172425214a0740611af9c306b
lukhen <lukasz.hen@gmail.com>
2021-06-09 23:56:51 +0200

    Moved some types and function to more reasonable location.

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,98 +1,16 @@
 import * as E from "fp-ts/lib/Either"
 import { pipe } from "fp-ts/lib/function"
 import * as O from "fp-ts/lib/Option"
-import { sequenceT } from "fp-ts/lib/Apply"
 import * as A from "fp-ts/lib/Array"
 import * as R from "fp-ts/lib/Record"
+import { Command1, Command2, Command3, comm1, comm2, comm3, defaultCommandMetas, CommandMetas } from "./command"
 
-
-interface Command1 {
-    _tag: "comm1",
-    arg: string
-    o1: string,
-    o2: string
-}
-
-// constructor
-function comm1(arg: string, o1: O.Option<Array<string>>, o2: O.Option<Array<string>>): E.Either<Error, Command1> {
-    return pipe(
-        sequenceT(O.option)(o1, o2),
-        O.fold(
-            () => E.left(Error("Option missing")),
-            (x: Array<Array<string>>) => E.right({
-                _tag: "comm1",
-                arg,
-                o1: x[0][0],
-                o2: x[1][0]
-            })
-        )
-    )
-}
-
-interface Command2 {
-    _tag: "comm2",
-    arg: string,
-    o3: string,
-    o4: string
-}
-
-// constructor
-function comm2(arg: string, o3: O.Option<Array<string>>, o4: O.Option<Array<string>>): E.Either<Error, Command2> {
-    return pipe(
-        sequenceT(O.option)(o3, o4),
-        O.fold(
-            () => E.left(Error("Option missing")),
-            (x: Array<Array<string>>) => E.right({
-                _tag: "comm2",
-                arg,
-                o3: x[0][0],
-                o4: x[1][0]
-            })
-        )
-    )
-}
-
-interface Command3 {
-    _tag: "comm3",
-    arg: string,
-    req: string,
-    opt: O.Option<Array<string>>
-}
-
-// constructor
-function comm3(arg: string, req: O.Option<Array<string>>, opt: O.Option<Array<string>>): E.Either<Error, Command3> {
-    return pipe(
-        req,
-        O.fold(
-            () => E.left(Error("Option missing")),
-            (value: Array<string>) => E.right({
-                _tag: "comm3",
-                arg,
-                req: value[0],
-                opt: opt
-            })
-        )
-    )
-}
 
 function getOpt(argv: string[]): (optName: string) => O.Option<Array<string>> {
     return optName => pipe(
         argv.findIndex(el => el == `--${optName}`),
         i => i == -1 ? O.none : O.some([argv[i + 1]])
     )
-}
-
-type CommandMetas = {
-    [key: string]: {
-        constructor: (arg: string, ...opts: Array<O.Option<Array<string>>>) => E.Either<Error, Command1 | Command2 | Command3>,
-        optNames: Array<string>
-    }
-}
-
-const defaultCommandMetas: CommandMetas = {
-    comm1: { constructor: comm1, optNames: ["o1", "o2"] },
-    comm2: { constructor: comm2, optNames: ["o3", "o4"] },
-    comm3: { constructor: comm3, optNames: ["req", "opt"] },
 }
 
 function parseArgv(argv: Array<string>, cms: CommandMetas): E.Either<Error, Command1 | Command2 | Command3> {
