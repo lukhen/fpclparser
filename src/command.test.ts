b9d72323b869db4bd512ec0188eb63902e039888
lukhen <lukasz.hen@gmail.com>
2021-06-17 07:32:30 +0200

    Command.fold works ok for Command1 with missing opiton o2.

--- src/command.test.ts
+++ src/command.test.ts
@@ -58,4 +58,29 @@ describe("Command fold", () => {
         )
     })
 
+    test("onCommand1: command name valid, arg valid, option o2 is missing", () => {
+        pipe(
+            C.xcomm1("comm1", "arg", { o1: ["asd"], o5: ["qewr"] }),
+            O.map(E.map(
+                (c: C.Command) => C.fold<string>({
+                    onCommand1: c => c._tag,
+                    onCommand2: c => c._tag,
+                    onCommand3: c => c._tag,
+                }, c))
+
+            ),
+            O.fold(
+                () => { fail(["y"]) },
+                e => pipe(
+                    e,
+                    E.fold(
+                        er => { expect(er).toEqual(Error("Option o2 is missing")) },
+                        a => { fail(["x"]) }
+                    )
+                )
+            )
+        )
+    })
+
+
 })
--- src/command.ts
+++ src/command.ts
@@ -2,6 +2,8 @@ import * as E from "fp-ts/lib/Either";
 import { pipe } from "fp-ts/lib/function";
 import * as O from "fp-ts/lib/Option";
 import { sequenceT } from "fp-ts/lib/Apply";
+import * as A from "fp-ts/lib/Array"
+import { Applicative2 } from "fp-ts/lib/Applicative";
 
 export interface Command1 {
     _tag: "comm1";
@@ -85,18 +87,36 @@ export const defaultCommandMetas: CommandMetas = {
 export type CommandOption = { name: string, values: string[] }
 export type CommandOptionDict = Record<string, string[]>
 
+const eitherApplicativeInstance: Applicative2<E.URI> = {
+    URI: E.URI,
+    ap: (fab, fa) => E.ap(fa)(fab),
+    map: (fa, f) => E.map(f)(fa),
+    of: E.of
+}
+
+
+function getCommand1Options(d: CommandOptionDict): Array<E.Either<Error, CommandOption>> {
+    return [
+        E.map((el: string[]) => ({ name: "o1", values: el }))(E.fromNullable(Error("Option o1 is missing"))(d["o1"])),
+        E.map((el: string[]) => ({ name: "o2", values: el }))(E.fromNullable(Error("Option o2 is missing"))(d["o2"])),
+    ]
+}
+
 export function xcomm1(name: string, arg: string, opts: CommandOptionDict): O.Option<E.Either<Error, Command1>> {
     return name != "comm1" ?
         O.none
         : O.some(pipe(
-            opts["o1"],
-            E.fromNullable(Error("Option o1 is missing")),
-            E.map(o1 => ({
-                _tag: "comm1",
-                arg: arg,
-                o1: o1[0],
-                o2: opts["o2"][0]
-            }))
+            opts,
+            getCommand1Options,
+            A.sequence(eitherApplicativeInstance),
+            E.map(
+                a => ({
+                    _tag: "comm1",
+                    arg: arg,
+                    o1: opts["o1"][0],
+                    o2: opts["o2"][0]
+                })
+            )
         ))
 }
 
