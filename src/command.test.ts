06f84ecf1480d45ddcd7a415882aca1b7fc03363
lukhen <lukasz.hen@gmail.com>
2021-06-21 08:23:47 +0200

    Now tests are more succinct and check just one thing.

--- src/command.test.ts
+++ src/command.test.ts
@@ -6,114 +6,6 @@ import { pipe } from "fp-ts/lib/function"
 import * as NEA from "fp-ts/lib/NonEmptyArray"
 
 describe("Command fold", () => {
-    function fail(msg: NEA.NonEmptyArray<string>): void {
-        expect(msg).toEqual([])
-    }
-
-    test("onCommand1: command name valid, arg valid, all options valid", () => {
-        pipe(
-            C.xcomm1("comm1", "arg", { o1: ["asd"], o2: ["qewr"] }),
-            O.map(E.map(
-                (c: C.Command) => C.fold<string>({
-                    onCommand1: c => c._tag,
-                    onCommand2: c => c._tag,
-                    onCommand3: c => c._tag,
-                }, c))
-
-            ),
-            O.fold(
-                () => { fail(["x"]) },
-                e => pipe(
-                    e,
-                    E.fold(
-                        er => { fail(["y"]) },
-                        a => { expect(a).toEqual("comm1") }
-                    )
-                )
-            )
-        )
-    })
-
-    test("onCommand1: command name valid, arg valid, option o1 is missing", () => {
-        pipe(
-            C.xcomm1("comm1", "arg", { o4: ["asd"], o2: ["qewr"] }),
-            O.map(E.map(
-                (c: C.Command) => C.fold<string>({
-                    onCommand1: c => c._tag,
-                    onCommand2: c => c._tag,
-                    onCommand3: c => c._tag,
-                }, c))
-
-            ),
-            O.fold(
-                () => { fail(["y"]) },
-                e => pipe(
-                    e,
-                    E.fold(
-                        er => { expect(er).toEqual(Error("Option o1 is missing")) },
-                        a => { fail(["x"]) }
-                    )
-                )
-            )
-        )
-    })
-
-    test("onCommand1: command name valid, arg valid, option o2 is missing", () => {
-        pipe(
-            C.xcomm1("comm1", "arg", { o1: ["asd"], o5: ["qewr"] }),
-            O.map(E.map(
-                (c: C.Command) => C.fold<string>({
-                    onCommand1: c => c._tag,
-                    onCommand2: c => c._tag,
-                    onCommand3: c => c._tag,
-                }, c))
-
-            ),
-            O.fold(
-                () => { fail(["y"]) },
-                e => pipe(
-                    e,
-                    E.fold(
-                        er => { expect(er).toEqual(Error("Option o2 is missing")) },
-                        a => { fail(["x"]) }
-                    )
-                )
-            )
-        )
-    })
-
-    test("onCommand1: command name valid, arg valid, option o1 and o2 are missing", () => {
-        pipe(
-            C.xcomm1("comm1", "arg", {}),
-            O.map(E.map(
-                (c: C.Command) => C.fold<string>({
-                    onCommand1: c => c._tag,
-                    onCommand2: c => c._tag,
-                    onCommand3: c => c._tag,
-                }, c))
-
-            ),
-            O.fold(
-                () => { fail(["y"]) },
-                e => pipe(
-                    e,
-                    E.fold(
-                        er => { expect(er).toEqual(Error("Option o1 is missing")) },
-                        a => { fail(["x"]) }
-                    )
-                )
-            )
-        )
-    })
-
-
-    test("onCommand1: command name invalid, arg valid, all options valid", () => {
-        pipe(
-            C.xcomm1("invalidcommadn", "arg", { o1: ["value3"], o2: ["value4"] }),
-            x => { expect(x).toEqual(O.none) }
-        )
-    })
-
 
     test("should produce command1 string for Command1", () => {
         pipe(
@@ -150,6 +42,55 @@ describe("Command fold", () => {
             x => { expect(x).toEqual("command3") }
         )
     })
+})
+
+describe("xcomm1", () => {
+    function fail(msg: NEA.NonEmptyArray<string>): void {
+        expect(msg).toEqual([])
+    }
+
+    test("command name valid, arg valid, all options valid", () => {
+        pipe(
+            C.xcomm1("comm1", "arg", { o1: ["asd"], o2: ["qewr"] }),
+            a => {
+                expect(a).toEqual(
+                    O.some(
+                        E.right({
+                            _tag: "comm1",
+                            arg: "arg",
+                            o1: "asd",
+                            o2: "qewr"
+                        } as C.Command1)))
+            }
+        )
+    })
+
+    test("command name valid, arg valid, option o1 is missing", () => {
+        pipe(
+            C.xcomm1("comm1", "arg", { o4: ["asd"], o2: ["qewr"] }),
+            er => { expect(er).toEqual(O.some(E.left(Error("Option o1 is missing")))) }
+        )
+    })
+
+    test("command name valid, arg valid, option o2 is missing", () => {
+        pipe(
+            C.xcomm1("comm1", "arg", { o1: ["asd"], o5: ["qewr"] }),
+            er => { expect(er).toEqual(O.some(E.left(Error("Option o2 is missing")))) }
+        )
+    })
+
+    test("command name valid, arg valid, option o1 and o2 are missing", () => {
+        pipe(
+            C.xcomm1("comm1", "arg", {}),
+            er => { expect(er).toEqual(O.some(E.left(Error("Option o1 is missing")))) }
+        )
+    })
 
 
+    test("command name invalid, arg valid, all options valid", () => {
+        pipe(
+            C.xcomm1("invalidcommadn", "arg", { o1: ["value3"], o2: ["value4"] }),
+            x => { expect(x).toEqual(O.none) }
+        )
+    })
 })
