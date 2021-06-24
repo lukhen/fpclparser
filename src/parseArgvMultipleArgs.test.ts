dbe7187e7c5e743b8cedc8affd23660a263c8a2b
lukhen <lukasz.hen@gmail.com>
2021-06-24 11:23:27 +0200

    Now we can produce a command with multiple args.

---
+++ src/parseArgvMultipleArgs.test.ts
@@ -0,0 +1,52 @@
+import { getAllOptionList, getOptionDict } from "./fpclparser"
+import { CommandOptionDict } from "./command"
+import * as E from "fp-ts/lib/Either";
+import { pipe } from "fp-ts/lib/function";
+import * as O from "fp-ts/lib/Option";
+import * as A from "fp-ts/lib/Array";
+
+interface CommandWithMultipleArgs {
+    _tag: "commwithmultipleargs",
+    arg1: string,
+    arg2: string,
+    opt1: string,
+    opt2: string
+}
+
+type Command = O.Option<E.Either<Error, CommandWithMultipleArgs>>
+
+type CommMultipleArgs = (name: string, arg: string[], opts: CommandOptionDict) => Command;
+
+function parseArgvMultipleArgs(argv: Array<string>, comms: CommMultipleArgs[]): Command {
+    return pipe(
+        comms,
+        A.map(comm => comm(argv[0], getArgs(argv), getOptionDict(getAllOptionList(argv)))),
+        x => A.filter(O.isSome)(x),
+        x => A.isEmpty(x) ? O.none : x[0]
+    );
+}
+
+// !!!
+const commWithArgList: CommMultipleArgs = (name, arg, opts) => {
+    return O.some(E.right({
+        _tag: "commwithmultipleargs",
+        arg1: "",
+        arg2: "",
+        opt1: "",
+        opt2: ""
+    }))
+}
+
+// !!!
+export function getArgs(argv: string[]): string[] {
+    return [argv[1]]
+}
+
+describe("", () => {
+    test("", () => {
+        const argv: string[] = []
+        expect(parseArgvMultipleArgs(["commwithmultipleargs"], [commWithArgList])).toEqual(
+            commWithArgList("commwithmultipleargs", getArgs(argv), getOptionDict(getAllOptionList(argv)))
+        )
+    })
+})
