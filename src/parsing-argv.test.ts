aca4c324a018e2a655e658160d2c3ed94c9394a8
lukhen <lukasz.hen@gmail.com>
2021-08-03 08:37:32 +0200

    Now we can parse command name and args.

--- src/parsing-argv.test.ts
+++ src/parsing-argv.test.ts
@@ -1,7 +1,21 @@
 import { parseArgv2 } from "./fpclparser"
 
 describe("", () => {
-    test("", () => {
+    test("empty argv", () => {
         expect(parseArgv2([])).toEqual(["", [], {}])
     })
+
+    test("just name", () => {
+        expect(parseArgv2(["comm1"])).toEqual(["comm1", [], {}])
+    })
+
+    test("name, arg", () => {
+        expect(parseArgv2(["comm1", "arg"])).toEqual(["comm1", ["arg"], {}])
+    })
+
+    test("name, arg1, arg2", () => {
+        expect(parseArgv2(["comm1", "arg1", "arg2"])).toEqual(["comm1", ["arg1", "arg2"], {}])
+    })
+
+
 })
